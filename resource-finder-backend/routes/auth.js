270dc63cfe2daa5f1de6a5b4d68d8578c7243154
rangaraj.s <rangaraj.sam@gmail.com>
2025-06-14 22:01:24 +0530

    FRONTEND URL ADDED

--- resource-finder-backend/routes/auth.js
+++ resource-finder-backend/routes/auth.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 router.get('/auth/google', passport.authenticate('google', {scope:['profile']}))
 
-router.get('/auth/google/callback', passport.authenticate('google', {successRedirect:'http://localhost:5173', failureRedirect:'/login'}));
+router.get('/auth/google/callback', passport.authenticate('google', {successRedirect:process.env.FRONTEND_URL, failureRedirect:'/login'}));
 
 router.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
-router.get('/auth/github/callback', passport.authenticate('github', { successRedirect: 'http://localhost:5173', failureRedirect: '/login' }));
+router.get('/auth/github/callback', passport.authenticate('github', { successRedirect: process.env.FRONTEND_URL, failureRedirect: '/login' }));
 
 router.get('/auth/logout', (req, res) => {
     req.logout(() => {
